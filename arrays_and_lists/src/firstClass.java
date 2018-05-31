a0567833bc2691476ed98b371998adcf45923608
Alexander Rusev <alexanderrusev@abv.bg>
2018-05-31 17:19:45 +0300

    Adding 'symmetric array' and 'sum above diagonal'

--- arrays_and_lists/src/firstClass.java
+++ arrays_and_lists/src/firstClass.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
+
 
 public class firstClass {
+	public static void main(String[] args){
+		
+		// Is symmetric
+		
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt(); // брой числа в масива
+		in.nextLine();
+		
+		for(int i = 0; i < n; i++){
+			String[] array = in.nextLine().split(" ");
+			int length = array.length;
+			boolean isSymmetric = true;
+			
+			for (int j = 0; j < length / 2; j++) {
+				if(!array[j].equals(array[length - 1 - j])){
+					isSymmetric = false;
+					break;
+				}
+			}
+			
+			System.out.println(isSymmetric ? "Yes" : "No");
+		}
 
+	}
 }
---
+++ arrays_and_lists/src/multiDimensions.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class multiDimensions {
+	public static void main(String[] args){
+		
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		in.nextLine();
+		
+		int startCol = 0;
+		long sum = 0;
+		
+		long[][] matrix = new long[n][n]; //multi array
+		for(int i = 0; i < n; i++){
+			
+			for(int j = 0; j < n; j++){
+				matrix[i][j] =  (long) Math.pow(2, i + j);
+				if(j >= startCol){
+					sum += matrix[i][j];
+				}
+			}
+			
+			startCol++;
+		}
+		System.out.println(sum);
+	}
+}
