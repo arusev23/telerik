96423748ca6b47f3992348e9966605e97cdd2021
Alexander Rusev <alexanderrusev@abv.bg>
2018-06-01 14:55:46 +0300

    Fixed some things, begin of arrays and lists solving problems

---
+++ arrays_and_lists/src/arrayListDemo.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+
+public class arrayListDemo {
+	public static void main(String[] args){
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		
+		list.set(1, 4);
+		list.remove(1);
+		
+		for (Integer item : list) {
+		    System.out.println(item);
+		}
+		
+	}
+}
